fddd847732a9c8f165dd1c9882f81eac070edc1d
JorgeLGA <106041997+JorgeLGA@users.noreply.github.com>
2024-05-07 01:23:05 -0500

    Inicio

---
+++ src/app/modules/home/home.module.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+import { PeopleTableComponent } from '../../components';
+
+@NgModule({
+  declarations: [HomeComponent],
+  imports: [CommonModule, HomeRoutingModule, PeopleTableComponent]
+})
+export class HomeModule {}
